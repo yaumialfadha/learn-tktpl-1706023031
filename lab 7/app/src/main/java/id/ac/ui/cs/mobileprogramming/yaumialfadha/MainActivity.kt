b4b4b046670795cde507bb86f12cd4ed74ec1f2b
yaumialfadha <yaumialfadha28@gmail.com>
2020-12-12 15:55:41 +0700

    lab-7

---
+++ lab 7/app/src/main/java/id/ac/ui/cs/mobileprogramming/yaumialfadha/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.mobileprogramming.yaumialfadha
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.ac.ui.cs.mobileprogramming.yaumialfadha.R
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
